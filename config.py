faddd452a53e387bf77870b5effc6a99e9564eb9
Steven Lee <gyl_mail@qq.com>
2019-07-04 15:54:49 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ log_path = os.path.join(basedir,'log')
 epochs = 300
 anchors=[[0.57273/13, 0.677385/13], [1.87446/13, 2.06253/13], [3.33843/13, 5.47434/13], [7.88282/13, 3.52778/13], [9.77052/13, 9.16828/13]]
 model_path = os.path.join(basedir,'mdl','mdl')
-vocimdir = r"D:\Users\yl_gong\Desktop\dl\voc\combo\image"
-voclabeldir= r"D:\Users\yl_gong\Desktop\dl\voc\combo\label"
+vocimdir = r"D:\Users\xxx\Desktop\dl\voc\combo\image"
+voclabeldir= r"D:\Users\xxx\Desktop\dl\voc\combo\label"
 trainbatch=15
 labels=['aeroplane','bicycle','bird','boat','bottle','bus','car','cat','chair','cow','diningtable','dog',
                         'horse','motorbike','person','pottedplant','sheep','sofa','train','tvmonitor']
